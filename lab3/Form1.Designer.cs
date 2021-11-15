e1d3dae8079d901e2b448064135d94707ce258e6
mia-im <62039441+mia-im@users.noreply.github.com>
2021-11-15 10:37:45 +0200

    Add files via upload

---
+++ lab3/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(43, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(714, 169);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Запустить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AboveNormal",
+            "BelowNormal",
+            "Highest",
+            "Lowest",
+            "Normal"});
+            this.comboBox1.Location = new System.Drawing.Point(43, 329);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "AboveNormal",
+            "BelowNormal",
+            "Highest",
+            "Lowest",
+            "Normal"});
+            this.comboBox2.Location = new System.Drawing.Point(544, 329);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 306);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Приоритет потока добавления";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(544, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Приоритет потока удаления";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(544, 236);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 27);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Строка для ввода";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(544, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Количество повторений";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ lab3/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        private static ManualResetEvent _stopper = new ManualResetEvent(false);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N = Convert.ToInt32(textBox2.Text); //переменная для количества вводимых строк
+
+            //создание первого потока для добавления строк
+            Thread thr1 = new Thread(() =>
+            {
+                for (int i=0; i<N; i++)
+                {
+                    richTextBox1.BeginInvoke(new Action(() =>
+                    {
+                        richTextBox1.Text += textBox1.Text+"\n";
+                    }));
+                    if (i==0) _stopper.Set(); //возобновление потока удаления после добавления одной строки
+                }
+            });
+
+            //создание второго потока для удаления последней строки
+            Thread thr2 = new Thread(() =>
+            {
+                _stopper.WaitOne(); //приостанавление потока для добавления строки
+                richTextBox1.BeginInvoke(new Action(() => 
+                {
+                    string[] result = richTextBox1.Lines.Where((x, y) => y != richTextBox1.Lines.Length - 2).ToArray();
+                    richTextBox1.Lines = result;
+                }));
+            });
+
+            //определение приоритета первого потока
+            string selectedPriority1 = comboBox1.SelectedItem.ToString();
+            if (selectedPriority1 == "AboveNormal") { thr1.Priority = ThreadPriority.AboveNormal; }
+            else if (selectedPriority1 == "BelowNormal") { thr1.Priority = ThreadPriority.BelowNormal; }
+            else if (selectedPriority1 == "Highest") { thr1.Priority = ThreadPriority.Highest; }
+            else if (selectedPriority1 == "Lowest") { thr1.Priority = ThreadPriority.Lowest; }
+            else { thr1.Priority = ThreadPriority.Normal; }
+
+            //определение приоритета второго потока
+            string selectedPriority2 = comboBox2.SelectedItem.ToString();
+            if (selectedPriority2 == "AboveNormal") { thr2.Priority = ThreadPriority.AboveNormal; }
+            else if (selectedPriority2 == "BelowNormal") { thr2.Priority = ThreadPriority.BelowNormal; }
+            else if (selectedPriority2 == "Highest") { thr2.Priority = ThreadPriority.Highest; }
+            else if (selectedPriority2 == "Lowest") { thr2.Priority = ThreadPriority.Lowest; }
+            else { thr2.Priority = ThreadPriority.Normal; }
+
+            //запуск потоков
+            thr1.Start();
+            thr2.Start();
+        }
+    }
+}
